6766b67471dd5dc2dfcfdebc98b4bc581a735df7
dmerniestic1987 <dmernies@uade.edu.ar>
2019-02-08 12:49:57 -0300

    Se mejora logging

--- client/lib/getWeb3.js
+++ client/lib/getWeb3.js
@@ -3,16 +3,14 @@ import Web3 from 'web3'
 const resolveWeb3 = (resolve) => {
   let { web3 } = window
   const alreadyInjected = typeof web3 !== 'undefined' // i.e. Mist/Metamask
-  const localProvider = `http://localhost:9545`
+  const localProvider = `http://localhost:8545`
 
   if (alreadyInjected) {
     console.log(`Injected web3 detected.`)
-    alert('Entro por already injected')
     web3 = new Web3(web3.currentProvider)
   } 
   else {
     console.log(`No web3 instance injected, using Local web3.`)
-    alert('Entro por else... local web')
     const provider = new Web3.providers.HttpProvider(localProvider)
     web3 = new Web3(provider)
   }
